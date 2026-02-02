fdeafcda2839c5d4e33d707d4ce55b5479fb949a
Faeze <faezeh.shabannejad@gmail.com>
2026-02-03 01:09:04 +0330

    javascript: WIP OOP & classes

--- Javascript/promises/promise/loadScript.js
+++ Javascript/promises/promise/loadScript.js
@@ -3,6 +3,7 @@ function loadScript(src) {
   return new Promise(function (resolve, reject) {
     let script = document.createElement("script");
     script.src = src;
+    script.type = "module"; //With type="module", let promise in script2.js is scoped to script2.js only and won’t conflict with any let promise in other files.
 
     script.onload = () => resolve(script);
     script.onerror = () => reject(`error occured in script ${src}`);
