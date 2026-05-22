d872a44dcff3825f46f74e6e55db58537a768d64
Faeze <faezeh.shabannejad@gmail.com>
2026-05-22 18:43:09 +0330

    enhance files: path alias via ps

---
+++ Javascript/init.ps1
@@ -0,0 +1,9 @@
+    function go-home { Set-Location "D:\GitHub\Learning-Journey-Intermediate_Frontend\Javascript\" }
+    function go-classes-colt { Set-Location ".\classes\coltSteele" }
+    function go-this-colt { Set-Location ".\thisKeyword\coltSteele" }
+    function run { node script.js }
+   function load-init { . "D:\GitHub\Learning-Journey-Intermediate_Frontend\Javascript\init.ps1" }
+
+    Write-Host "Project shortcuts loader" -ForegroundColor Green
+
+# . .\init.ps1
