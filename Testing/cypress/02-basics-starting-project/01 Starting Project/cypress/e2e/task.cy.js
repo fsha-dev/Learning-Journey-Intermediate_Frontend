643be7f3cfb576290a4e15259a8f523ba66a1f9f
Faeze <faezeh.shabannejad@gmail.com>
2026-06-15 16:59:54 +0330

    learn: cypress finish cypress basics

--- Testing/cypress/02-basics-starting-project/01 Starting Project/cypress/e2e/task.cy.js
+++ Testing/cypress/02-basics-starting-project/01 Starting Project/cypress/e2e/task.cy.js
@@ -32,4 +32,38 @@ describe("tasks management", () => {
     cy.get(".backdrop").should("exist");
     cy.get(".modal").should("exist");
   });
+  it("should filter tasks", () => {
+    cy.visit("http://localhost:5173/");
+    cy.contains("Add Task").click();
+    cy.get("#title").type("Learning Cypress 1");
+    cy.get("#summary").type("learn basics : get,contains,should,...");
+    cy.get("#category").select("urgent");
+    cy.get(".modal").contains("Add Task").click();
+    cy.get(".task").should("have.length", 1);
+    cy.get("#filter").select("moderate");
+    cy.get(".task").should("have.length", 0);
+    cy.get("#filter").select("urgent");
+    cy.get(".task").should("have.length", 1);
+    cy.get("#filter").select("all");
+    cy.get(".task").should("have.length", 1);
+  });
+  it("should add multiple tasks", () => {
+    cy.visit("http://localhost:5173/");
+    cy.contains("Add Task").click();
+    cy.get("#title").type("Task 1");
+    cy.get("#summary").type("learn basics : get,contains,should,...");
+    cy.get(".modal").contains("Add Task").click();
+    cy.get(".task").should("have.length", 1);
+
+    cy.contains("Add Task").click();
+    cy.get("#title").type("Task 2");
+    cy.get("#summary").type("learn basics : get,contains,should,...");
+    cy.get(".modal").contains("Add Task").click();
+    cy.get(".task").should("have.length", 2);
+
+    //Order
+    cy.get(".task").eq(0).contains("Task 1");
+    cy.get(".task").eq(1).contains("Task 2");
+  });
 });
+// cypress runs test in isolation, order of test is not matter
