f5272527e2438afb52e553b966024a1da6ca74b9
Faeze <faezeh.shabannejad@gmail.com>
2026-06-17 14:33:50 +0330

    edit test: modify error caused by assertion have.text & path items in menu

--- Testing/cypress/exercise/cypress/e2e/layout.cy.js
+++ Testing/cypress/exercise/cypress/e2e/layout.cy.js
@@ -9,31 +9,41 @@ describe("global layout", () => {
   });
   it("should display the home item in menu", () => {
     cy.visit("https://automationexercise.com/");
-    cy.get('li a[href="/"]').should("be.visible");
-    cy.get('li a[href="/"]').should("have.text", "Home");
+    cy.get(' .shop-menu ul li a[href="/"]').should("be.visible");
+    cy.get('.shop-menu ul li a[href="/"]').should("contain.text", "Home");
   });
   it("should display the products item in menu", () => {
     cy.visit("https://automationexercise.com/");
-    cy.get('li a[href="/products]').should("be.visible");
-    cy.get('li a[href="/products]').should("have.text", "Products");
+    cy.get('.shop-menu ul li a[href="/products]').should("be.visible");
+    cy.get('.shop-menu ul li a[href="/products]').should(
+      "contain.text",
+      "Products",
+    );
   });
   it("should display the cart item in menu", () => {
     cy.visit("https://automationexercise.com/");
-    cy.get('li a[href="/view_cart]').should("be.visible");
-    cy.get('li a[href="/view_cart]').should("have.text", "Cart");
+    cy.get('.shop-menu ul li a[href="/view_cart]').should("be.visible");
+    cy.get('.shop-menu ul li a[href="/view_cart]').should(
+      "contain.text",
+      "Cart",
+    );
   });
   it("should display the contact item in menu", () => {
     cy.visit("https://automationexercise.com/");
-    cy.get('li a[href="/contact_us]').should("be.visible");
-    cy.get('li a[href="/contact_us]').should("have.text", "Contact us");
+    cy.get('.shop-menu ul li a[href="/contact_us]').should("be.visible");
+    cy.get('.shop-menu ul li a[href="/contact_us]').should(
+      "contain.text",
+      "Contact us",
+    );
   });
   it("should display the product sections", () => {
+    cy.visit("https://automationexercise.com/");
     cy.get(".features_items").should("be.visible");
     cy.get(".features_items h2").should("contain.text", "Features Items");
   });
-  it("should display the slider in the main page", () => {
+  it("should display the s.shop-menu ul lider in the main page", () => {
     cy.visit("https://automationexercise.com/");
-    cy.get("#slider").should("be.visible");
+    cy.get("#s.shop-menu ul lider").should("be.visible");
   });
   it("should display the footer", () => {
     cy.visit("https://automationexercise.com/");
