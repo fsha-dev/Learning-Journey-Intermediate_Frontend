a6db36fd2b4395732adf88b6ea8dca1f7dde508b
Faeze <faezeh.shabannejad@gmail.com>
2026-06-17 12:51:21 +0330

    add: config autocomplete, ongoing: tests

--- Testing/cypress/exercise/cypress/e2e/layout.cy.js
+++ Testing/cypress/exercise/cypress/e2e/layout.cy.js
@@ -1,5 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("global layout", () => {
+  it("should display the site logo", () => {
+    cy.visit("https://automationexercise.com/");
+    cy.get('img[src *="logo"]').as("logoImage");
+    cy.get("@logoImage").should("exist");
+  });
+  it("should display the menu", () => {
+    cy.visit("https://automationexercise.com/");
+    cy.get('[class *="shop-menu"]').as("mainMenu");
+    cy.get("@mainMenu").should("exist");
+  });
+  it("should display the product sections", () => {
+    cy.visit("https://automationexercise.com/");
+    cy.get(".features_items").should("exist");
+    cy.get(".features_items h2").should("contain.text", "Features Items");
+  });
+});
