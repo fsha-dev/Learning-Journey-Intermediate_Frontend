44f9223fc60dbb8518d181ffbb2f59d54c410482
Faeze <faezeh.shabannejad@gmail.com>
2026-06-17 13:52:25 +0330

    complete task test: layout global ui

--- Testing/cypress/exercise/cypress.config.js
+++ Testing/cypress/exercise/cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'zvd6vt',
   allowCypressEnv: false,
 
   e2e: {
--- Testing/cypress/exercise/cypress/e2e/layout.cy.js
+++ Testing/cypress/exercise/cypress/e2e/layout.cy.js
@@ -1,17 +1,42 @@
 describe("global layout", () => {
   it("should display the site logo", () => {
     cy.visit("https://automationexercise.com/");
-    cy.get('img[src *="logo"]').as("logoImage");
-    cy.get("@logoImage").should("exist");
+    cy.get('img[src *="logo"]').should("be.visible");
   });
   it("should display the menu", () => {
     cy.visit("https://automationexercise.com/");
-    cy.get('[class *="shop-menu"]').as("mainMenu");
-    cy.get("@mainMenu").should("exist");
+    cy.get(".shop-menu").should("be.visible");
+  });
+  it("should display the home item in menu", () => {
+    cy.visit("https://automationexercise.com/");
+    cy.get('li a[href="/"]').should("be.visible");
+    cy.get('li a[href="/"]').should("have.text", "Home");
+  });
+  it("should display the products item in menu", () => {
+    cy.visit("https://automationexercise.com/");
+    cy.get('li a[href="/products]').should("be.visible");
+    cy.get('li a[href="/products]').should("have.text", "Products");
+  });
+  it("should display the cart item in menu", () => {
+    cy.visit("https://automationexercise.com/");
+    cy.get('li a[href="/view_cart]').should("be.visible");
+    cy.get('li a[href="/view_cart]').should("have.text", "Cart");
+  });
+  it("should display the contact item in menu", () => {
+    cy.visit("https://automationexercise.com/");
+    cy.get('li a[href="/contact_us]').should("be.visible");
+    cy.get('li a[href="/contact_us]').should("have.text", "Contact us");
   });
   it("should display the product sections", () => {
-    cy.visit("https://automationexercise.com/");
-    cy.get(".features_items").should("exist");
+    cy.get(".features_items").should("be.visible");
     cy.get(".features_items h2").should("contain.text", "Features Items");
+  });
+  it("should display the slider in the main page", () => {
+    cy.visit("https://automationexercise.com/");
+    cy.get("#slider").should("be.visible");
+  });
+  it("should display the footer", () => {
+    cy.visit("https://automationexercise.com/");
+    cy.get("#footer").should("be.visible");
   });
 });
