3c2ee76edc8c54cebe0aaf6f6da820aba5b18c52
Faeze <faezeh.shabannejad@gmail.com>
2026-07-14 12:37:09 +0330

    cypress: solved error(get window, disabled btn and fetching)

--- Testing/cypress/05-stubs-starting-project/01 Starting Project/cypress/e2e/location.cy.js
+++ Testing/cypress/05-stubs-starting-project/01 Starting Project/cypress/e2e/location.cy.js
@@ -2,23 +2,30 @@
 
 describe("share location", () => {
   it("should fetch the user location", () => {
-    cy.visit("/");
-    cy.stub(window.navigator.geolocation, "getCurrentPosition")
-      .as("getUserPosition")
-      .callsFake((callBack) => {
-        callBack({
-          coords: {
-            latitude: 37.5,
-            logitude: 48.1,
-          },
+    cy.visit("/").then((window) => {
+      cy.stub(window.navigator.geolocation, "getCurrentPosition")
+        .as("getUserPosition")
+        .callsFake((callBack) => {
+          setTimeout(() => {
+            callBack({
+              coords: {
+                latitude: 37.5,
+                logitude: 48.1,
+              },
+            });
+          }, 300);
         });
-      });
+    });
+
     cy.get('[data-cy="get-loc-btn"]').as("lock-btn").click();
     cy.get("@getUserPosition").should("have.been.called");
     // when button start fetching; button disables
-    cy.get("lock-btn").should("not.exist");
+    cy.get("@lock-btn").should("have.attr", "disabled");
+    cy.get(".loader").should("be.visible");
+    // when fetched; button disappear
+    cy.get("@lock-btn").should("not.exist");
     cy.get('[data-cy="actions"]')
       .should("be.visible")
-      .and("have.text", "Location fetched!");
+      .and("contain.text", "Location fetched!");
   });
 });
