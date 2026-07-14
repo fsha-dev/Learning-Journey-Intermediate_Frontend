4ddb4d09716cc959b4b5503567a98c4264b66644
Faeze <faezeh.shabannejad@gmail.com>
2026-07-14 17:09:32 +0330

    cypress: add step share location

--- Testing/cypress/05-stubs-starting-project/01 Starting Project/cypress/e2e/location.cy.js
+++ Testing/cypress/05-stubs-starting-project/01 Starting Project/cypress/e2e/location.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe("share location", () => {
-  it("should fetch the user location", () => {
+  beforeEach(() => {
     cy.visit("/").then((window) => {
       cy.stub(window.navigator.geolocation, "getCurrentPosition")
         .as("getUserPosition")
@@ -16,7 +16,8 @@ describe("share location", () => {
           }, 300);
         });
     });
-
+  });
+  it("should fetch the user location", () => {
     cy.get('[data-cy="get-loc-btn"]').as("lock-btn").click();
     cy.get("@getUserPosition").should("have.been.called");
     // when button start fetching; button disables
@@ -27,5 +28,20 @@ describe("share location", () => {
     cy.get('[data-cy="actions"]')
       .should("be.visible")
       .and("contain.text", "Location fetched!");
+  });
+  it("share location : shows an error when the name is missing", () => {
+    cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get('[data-cy="share-loc-btn"]').as("share-btn").click();
+    cy.get("#error")
+      .should("be.visible")
+      .and("contain.text", "enter your name and get your location first");
+  });
+  it("should share location", () => {
+    cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get('[data-cy="name-input"]')
+      .type("Dummy Name")
+      .should("have.value", "Dummy Name");
+    cy.window().stub("navigator.clipboard");
+    cy.get('[data-cy="share-loc-btn"]').as("share-btn").click();
   });
 });
