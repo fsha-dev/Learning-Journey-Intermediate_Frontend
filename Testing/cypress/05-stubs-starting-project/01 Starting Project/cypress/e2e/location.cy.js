33b33da44ce62673e0a3f3843a0c9e6cf8ed1381
Faeze <faezeh.shabannejad@gmail.com>
2026-07-14 12:14:38 +0330

    cypress: stub + with error

--- Testing/cypress/05-stubs-starting-project/01 Starting Project/cypress/e2e/location.cy.js
+++ Testing/cypress/05-stubs-starting-project/01 Starting Project/cypress/e2e/location.cy.js
@@ -1,8 +1,24 @@
 /// <reference types="cypress" />
 
-describe('share location', () => {
-  it('should fetch the user location', () => {
-    cy.visit('/');
-    cy.get('[data-cy="get-loc-btn"]').click();
+describe("share location", () => {
+  it("should fetch the user location", () => {
+    cy.visit("/");
+    cy.stub(window.navigator.geolocation, "getCurrentPosition")
+      .as("getUserPosition")
+      .callsFake((callBack) => {
+        callBack({
+          coords: {
+            latitude: 37.5,
+            logitude: 48.1,
+          },
+        });
+      });
+    cy.get('[data-cy="get-loc-btn"]').as("lock-btn").click();
+    cy.get("@getUserPosition").should("have.been.called");
+    // when button start fetching; button disables
+    cy.get("lock-btn").should("not.exist");
+    cy.get('[data-cy="actions"]')
+      .should("be.visible")
+      .and("have.text", "Location fetched!");
   });
 });
